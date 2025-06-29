53e35a6cf836959cdcd88d0f68e30d7ea9f8667f
chxnmay17 <chxnmay17@gmail.com>
2025-06-29 23:42:22 +0530

    Fixed margin issue

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"], // tells Tailwind to scan your HTML files
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
