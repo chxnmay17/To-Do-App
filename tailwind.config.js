30b4e50802ed38d3dbb2338837f34e02f6e09edc
chxnmay17 <chxnmay17@gmail.com>
2025-06-30 21:54:28 +0530

    worked on the UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./*.html"], // tells Tailwind to scan your HTML files
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: [".todo.html"], // tells Tailwind to scan your HTML files
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   }
   
